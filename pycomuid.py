35f03233890e6401ea044b84a58717bba263521c
Lixin Zhan <lixinzhan@gmail.com>
2026-04-14 09:59:52 -0400

    ai assisted optimization of pycomuid.py

--- pycomuid.py
+++ pycomuid.py
@@ -12,23 +12,15 @@
 
 import os
 from datetime import datetime
-from random import random
+import random
 
 def get_dicomuid():
     uid_root = '9.9.9.9.9.9.9999.999.9.1.'
 
-    uid = uid_root
+    now = datetime.now()
+    time_str = now.strftime('%Y%m%d%H%M%S') + f"{now.microsecond:06d}"
+    rand_str = f"{random.randint(0, 999999999):09d}"
 
-    t = str(datetime.now())
-    year = t[0:4]
-    month = t[5:7]
-    day = t[8:10]
-    hr = t[11:13]
-    min = t[14:16]
-    sec = t[17:19]
-    msec = repr(datetime.now().microsecond).zfill(6)
-    rand = repr(int(random()*10**9)).zfill(9)
-
-    uid = uid_root+year+month+day+hr+min+sec+msec+rand
+    uid = uid_root + time_str + rand_str
     
     return uid
